c3b3614d0b7418ce3df38220e633a4ee522ace9f
padamupreti <59172926+padamupreti@users.noreply.github.com>
2025-06-10 13:44:33 +0545

    Add timer functionality

--- src/components/HeroTimers.jsx
+++ src/components/HeroTimers.jsx
@@ -1,13 +1,28 @@
-import TimerReg from "./TimerReg"
-import TimerIngenium from "./TimerIngenium"
+import TimerCom from "./TimerCom"
 
 function HeroTimers() {
+    const registerClosure = new Date(2025, 5, 28)
+    const mainEventStart = new Date(2025, 6, 3, 9, 0, 0)
+
+    console.log(`Registration by: ${registerClosure.toString()}`)
+    console.log(`Main Event Date: ${mainEventStart.toString()}`)
+
     return (
         <div className="flex flex-col space-y-10 md:space-y-14 relative z-0">
             <div className="h-38 w-46 md:h-48 md:w-56 rounded-full bg-teal-400/10 blur-xl absolute -z-10 pointer-events-none top-0 -left-20"></div>
-            <TimerReg />
+            <TimerCom
+                title={"Registration Ends In"}
+                expireTitle={"Registration Closed"}
+                expireDate={"June 28"}
+                expiryTimestamp={registerClosure}
+            />
             <div className="bg-gradient-to-r from-transparent via-teal-400/90 to-transparent h-1 w-2/3 mx-auto -translate-y-2.5"></div>
-            <TimerIngenium />
+            <TimerCom
+                title={"Main Event Starts In"}
+                expireTitle={"Main Event Started"}
+                expireDate={"July 03"}
+                expiryTimestamp={mainEventStart}
+            />
         </div>
     )
 }
--- src/components/TimerCom.jsx
+++ src/components/TimerCom.jsx
@@ -1,32 +1,50 @@
-function TimerIngenium() {
+import { useState } from "react"
+import { useTimer } from "react-timer-hook"
+
+function TimerCom({ title, expireTitle, expireDate, expiryTimestamp }) {
+    const parts = expireDate.split(" ")
+
+    const [expired, setExpired] = useState(false)
+    const { minutes, hours, days } = useTimer({
+        expiryTimestamp,
+        onExpire: () => setExpired(true),
+        interval: 20,
+    })
+
     return (
         <div className="relative">
             <span className="absolute -z-20 text-gray-950 text-outline text-4xl sm:text-5xl -translate-y-2/3">
-                July 03
+                {expireDate}
             </span>
             <div className="flex items-center space-x-5 bg-slate-100/5 backdrop-blur-xs px-2 sm:px-4 py-1 sm:py-2 border border-white/10 rounded-xl relative">
                 <span className="absolute -top-4 -right-7 font-medium capitalize text-xs sm:text-sm px-2 py-1 bg-teal-400/90 text-gray-950 border rounded-full">
-                    Main event starts in
+                    {expired ? expireTitle : title}
                 </span>
                 <span className="text-teal-400/90 font-bold text-center text-2xl xs:text-4xl pr-3 mr-3 border-r border-white/10">
-                    July
+                    {parts[0]}
                     <br />
-                    03
+                    {parts[1]}
                 </span>
                 <div className="font-medium text-center flex flex-col space-y-1">
-                    <span className="text-3xl xs:text-5xl">24</span>
+                    <span className="text-3xl xs:text-5xl">
+                        {days.toString().padStart(2, "0")}
+                    </span>
                     <span className="text-teal-400/90 text-xs sm:text-sm uppercase">
                         Days
                     </span>
                 </div>
                 <div className="font-medium text-center flex flex-col space-y-1">
-                    <span className="text-3xl xs:text-5xl">09</span>
+                    <span className="text-3xl xs:text-5xl">
+                        {hours.toString().padStart(2, "0")}
+                    </span>
                     <span className="text-teal-400/90 text-xs sm:text-sm uppercase">
                         Hours
                     </span>
                 </div>
                 <div className="font-medium text-center flex flex-col space-y-1">
-                    <span className="text-3xl xs:text-5xl">35</span>
+                    <span className="text-3xl xs:text-5xl">
+                        {minutes.toString().padStart(2, "0")}
+                    </span>
                     <span className="text-teal-400/90 text-xs sm:text-sm uppercase">
                         Minutes
                     </span>
@@ -36,4 +54,4 @@ function TimerIngenium() {
     )
 }
 
-export default TimerIngenium
+export default TimerCom

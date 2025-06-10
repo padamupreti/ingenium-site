55fc2a9252d79fb91471fc4ea44b53a30c0e7a56
padamupreti <59172926+padamupreti@users.noreply.github.com>
2025-06-10 21:39:13 +0545

    Add minor change to Hero section min-height

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <section
             id="register"
-            className="bg-gray-950/60 text-white flex flex-col gap-16 justify-evenly items-start pt-12 md:pt-0 xl:pt-20 xl:pb-14 px-6 xs:px-8 sm:px-10 md:items-center md:flex-row relative max-xl:min-h-dvh"
+            className="bg-gray-950/60 text-white flex flex-col gap-16 justify-evenly items-start pt-12 md:pt-0 xl:pt-20 xl:pb-14 px-6 xs:px-8 sm:px-10 md:items-center md:flex-row relative max-xl:min-h-vh"
         >
             <HeroAction />
             <HeroTimers />
